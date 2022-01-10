8bbfa057207195e4f84f9f8f069c435666526161
que <qs5779@mail.com>
2022-01-10 16:20:23 -0700

    Adding setup

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Wtf tools provide base functionality for various automation and administration activities.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='wtftools',
+    version='0.1.0',
+    url='https://github.com/qs5779/python-wtftools',
+    license='BSD',
+    author='Quien Sabe',
+    author_email='qs5779@mail.com',
+    description='Wtf tools provide base functionality for various automation and administration activities.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'wtflogger = wtftools.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
